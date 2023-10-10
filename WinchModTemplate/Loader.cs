14e801ed0bfe6a1f2b7c87791d1b443e46e25d5c
Nick <nicholasconnors@hotmail.ca>
2023-10-10 14:18:59 -0400

    Implement mod template

---
+++ WinchModTemplate/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace WinchModTemplate
+{
+	public class Loader
+	{
+		/// <summary>
+		/// This method is run by Winch to initialize your mod
+		/// </summary>
+		public static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(WinchModTemplate));
+			gameObject.AddComponent<WinchModTemplate>();
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
---
+++ WinchModTemplate/WinchModTemplate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace WinchModTemplate
+{
+	public class WinchModTemplate : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(WinchModTemplate)} has loaded!");
+		}
+	}
+}
